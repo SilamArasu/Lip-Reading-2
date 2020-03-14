7f2286038fb7be4d3954135efbe8cd6855e4fd8d
SilamArasu <silam.7a@gmail.com>
2020-03-14 23:24:32 +0530

    spell done

--- spell.py
+++ spell.py
@@ -2,40 +2,20 @@ import re
 import string
 from collections import Counter
 
-# Source: https://github.com/commonsense/metanl/blob/master/metanl/token_utils.py
-def untokenize(words):
-    """
-    Untokenizing a text undoes the tokenizing operation, restoring
-    punctuation and spaces to the places that people expect them to be.
-    Ideally, `untokenize(tokenize(text))` should be identical to `text`,
-    except for line breaks.
-    """
-    text = ' '.join(words)
-    step1 = text.replace("`` ", '"').replace(" ''", '"').replace('. . .',  '...')
-    step2 = step1.replace(" ( ", " (").replace(" ) ", ") ")
-    step3 = re.sub(r' ([.,:;?!%]+)([ \'"`])', r"\1\2", step2)
-    step4 = re.sub(r' ([.,:;?!%]+)$', r"\1", step3)
-    step5 = step4.replace(" '", "'").replace(" n't", "n't").replace(
-         "can not", "cannot")
-    step6 = step5.replace(" ` ", " '")
-    return step6.strip()
-
-# Source: https://stackoverflow.com/questions/367155/splitting-a-string-into-words-and-punctuation
-def tokenize(text):
-    return re.findall(r"\w+|[^\w\s]", text, re.UNICODE)
+# from spell import Spell
+path = '/home/arasu/FYP/LipReadingGUI/Training/dictionaries/grid.txt'
 
 # Source: http://norvig.com/spell-correct.html (with some modifications)
 class Spell(object):
     def __init__(self, path):
-        self.dictionary = Counter(list(string.punctuation) + self.words(open(path).read()))
+        self.dictionary = Counter(self.words(open(path).read()))
 
     def words(self, text):
         return re.findall(r'\w+', text.lower())
 
     def P(self, word, N=None):
         "Probability of `word`."
-        if N is None:
-            N = sum(self.dictionary.values())
+        N = sum(self.dictionary.values())
         return self.dictionary[word] / N
 
     def correction(self, word):
@@ -44,7 +24,16 @@ class Spell(object):
 
     def candidates(self, word):
         "Generate possible spelling corrections for word."
-        return (self.known([word]) or self.known(self.edits1(word)) or self.known(self.edits2(word)) or [word])
+        known = self.known([word])
+        if len(known) != 0:
+                return known
+        ed1 = self.known(self.edits1(word))
+        if len(ed1) != 0:
+                return ed1
+        ed2 = self.known((e2 for e1 in self.edits1(word) for e2 in self.edits1(e1)))
+        if len(ed2) != 0:
+                return ed2
+        return known
 
     def known(self, words):
         "The subset of `words` that appear in the dictionary of WORDS."
@@ -60,14 +49,10 @@ class Spell(object):
         inserts    = [L + c + R               for L, R in splits for c in letters]
         return set(deletes + transposes + replaces + inserts)
 
-    def edits2(self, word):
-        "All edits that are two edits away from `word`."
-        return (e2 for e1 in self.edits1(word) for e2 in self.edits1(e1))
-
     # Correct words
     def corrections(self, words):
         return [self.correction(word) for word in words]
 
     # Correct sentence
     def sentence(self, sentence):
-        return untokenize(self.corrections(tokenize(sentence)))
+        return ' '.join(self.corrections(sentence.split()))
