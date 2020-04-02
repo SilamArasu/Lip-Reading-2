f406d0ce325b20085593a18aa563e5c43d5ce8f9
SilamArasu <silam.7a@gmail.com>
2020-04-02 11:36:23 +0530

    augmentation done

--- vidaug.py
+++ vidaug.py
@@ -3,15 +3,27 @@ from PIL import Image
 from scipy.ndimage.filters import gaussian_filter
 import numpy as np
 
-def generalize(frames):
-    frames = (frames - frames.min(axis=0)) / frames.std(axis=0)
+def normalize(frames):
+    """
+    Changing the range of pixel intensity values to mean 0
+    """
+    # print("Executing normalize")
+    frames = (frames - frames.min()) / frames.std()
     return frames
 
 def vertical_flip(frames):
+    """
+    Vertically flip the video
+    """
+    # print("Executing vertical_flip")
     frames = np.flip(frames, 1)
     return frames
 
 def pixelate(frames):
+    """
+    Downscale the image by the pixelation factor(75%) and then upscale it with nearest neighbour to original size
+    """
+    # print("Executing pixelate")
     for i in range(len(frames)):
         img = Image.fromarray(frames[i])
         imgSmall = img.resize((38,75),resample=Image.BILINEAR) # Original shape=(50,100). Resizing to 75%
@@ -19,13 +31,25 @@ def pixelate(frames):
     return np.array(frames)
 
 def blur(frames):
+    """
+    Blur images using gaussian filter
+    """
+    # print("Executing blur")
     blurred = [gaussian_filter(x, sigma=1) for x in frames]
     return np.array(blurred)
 
 def invert(frames):
+    """
+    Inverts the color of the video
+    """
+    # print("Executing invert")
     return np.invert(frames)
 
 def add(frames):
+    # print("Executing add")
+    """
+    Add a value to all pixel intesities in an video.
+    """
     data_final = []
     for i in range(len(frames)):
         image = frames[i].astype(np.int32)
@@ -37,6 +61,10 @@ def add(frames):
     return np.array(data_final)
 
 def multiply(frames):
+    # print("Executing multiply")
+    """
+    Multiply all pixel intensities with given value.
+    """
     data_final = []
     for i in range(len(frames)):
         image = frames[i].astype(np.float64)
@@ -47,6 +75,22 @@ def multiply(frames):
         data_final.append(image.astype(np.uint8))
     return np.array(data_final)
 
+def salt_pepper(frames):
+    # print("Executing salt_pepper")
+    """
+    Salt - This sets a certain fraction of pixel intesities to 255 using noise, hence they become white.
+    Pepper - This sets a certain fraction of pixel intesities to 255 using noise, hence they become black.
+    """
+    val = random.choice([0,255]) # Choose either black or white randomly
+    data_final = []
+    for i in range(len(frames)):
+        img = frames[i].astype(np.float)
+        noise = np.random.randint(50, size=img.shape).astype(np.float)
+        img = np.where(noise == 0, val, img)
+        data_final.append(img.astype(np.uint8))
+    return np.array(data_final)
+
 def select_augmentation(frames):
-    func = random.choice([generalize, vertical_flip, pixelate])
+    # Randomly selects a augmentation to be applied
+    func = random.choice([normalize, vertical_flip, pixelate, blur, invert, add, multiply, salt_pepper])
     return func(frames)
